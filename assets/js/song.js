8d5baf4c6774bbd4182727a44aa22324c8e8307e
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-08-27 21:09:03 -0600

    add second api functionality and playlist creation

--- assets/js/song.js
+++ assets/js/song.js
@@ -7,32 +7,35 @@ let lyricsEmbedScriptEl = document.getElementById('lyrics-embed-script');
 // --- LocalStorage: Start --- //
 // get song details from localStorage
 function getSong() {
-    console.log('getSong');
     return JSON.parse(localStorage.getItem('song'));
 }
 // --- LocalStorage: End --- //
 
 
 // --- APIs: Start --- //
-function fetchSongSamples(songDetails) {
-    let a = encodeURIComponent(songDetails.artist);
-    let s = encodeURIComponent(songDetails.song);
+function fetchSongPlaylist(playerId) {
+    let songPlaylist = playerId;
 
     const sampledOptions = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '4a8cf8b268msh8770afef74aa77ep128dcdjsnd6101df11545',
-            'X-RapidAPI-Host': 'whosampled-api.p.rapidapi.com'
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
         }
     };
 
-    let songMetaDataAPI = `https://whosampled-api.p.rapidapi.com/${a}/${s}/samples`;
+    let songMetaDataAPI = `https://spotify81.p.rapidapi.com/seed_to_playlist?uri=spotify%3Atrack%3A${songPlaylist}`;
 
     return fetch(songMetaDataAPI, sampledOptions)
         .then(response => response.json())
         .then((data) => {
+            console.log('data');
             console.log(data);
-            return data;
+            let playlist = data.mediaItems[0].uri.split(':');
+            let playlistId = playlist.pop() || playlist.pop();
+            console.log(`added playlistId: ${playlistId}`);
+            return playlistId;
+
         })
         .catch(err => console.log(err));
 
@@ -55,15 +58,21 @@ function displaySongPlayer(playerId) {
 
 function displaySongVideo(songDetails) {
     let embedUrl = songDetails.videoId.replace('watch?v=', 'embed/');
-    console.log(embedUrl);
     postscribe('#song-player', `<iframe width="560" height="315" src="${embedUrl}" title="${songDetails.song}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+
+}
+
+async function displaySongplaylist(playerId) {
+    let playlistId = await fetchSongPlaylist(playerId);
+    console.log('playlistID');
+    console.log(playlistId);
+    postscribe('#song-player', `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`);
 
 }
 
 // --- Display Song Page: Start --- //
 function displaySongDetails() {
     let songDetails = getSong();
-    console.log(songDetails);
 
     if (!songDetails.songId) {
         console.log('There was an issue getting the song data');
@@ -72,13 +81,12 @@ function displaySongDetails() {
 
     if (songDetails.playerId) {
         displaySongPlayer(songDetails.playerId);
+        displaySongplaylist(songDetails.playerId);
 
     } else if (songDetails.videoId) {
         displaySongVideo(songDetails);
     }
     displaySongLyrics(songDetails.songId);
-
-    fetchSongSamples(songDetails);
 }
 
 
